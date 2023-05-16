dc296c090e6475843bb2f144fff969dec9f627ae
Timothe Censier <tcensier@f1r2s9.codam.nl>
2023-05-16 21:49:14 +0200

    done for today

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,19 +6,17 @@
 /*   By: tcensier <tcensier@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/16 17:06:35 by tcensier      #+#    #+#                 */
-/*   Updated: 2023/05/16 17:08:26 by tcensier      ########   odam.nl         */
+/*   Updated: 2023/05/16 20:38:56 by tcensier      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 # include "../MLX42/include/MLX42/MLX42.h"
-# include <stdlib.h>
+# include "../libft/includes/libft.h"
 # include <stdio.h>
-# include <unistd.h>
-# define WIDTH 256
-# define HEIGHT 256
-
-
+# define WIDTH 960
+# define HEIGHT 540
+# define BPP sizeof(int32_t)
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,44 +6,40 @@
 /*   By: tcensier <tcensier@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/16 17:06:49 by tcensier      #+#    #+#                 */
-/*   Updated: 2023/05/16 18:12:28 by tcensier      ########   odam.nl         */
+/*   Updated: 2023/05/16 21:49:02 by tcensier      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
+//-------------------------------------------------------------------------
 
-static void	ft_error(void)
+int32_t	get_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
 {
-	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
-	exit(EXIT_FAILURE);
+	return (r << 24 | g << 16 | b << 8 | a);
 }
 
-static void	ft_hook(void *param)
+void	global_hook(void *param)
 {
-	const mlx_t*	mlx = param;
-	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
+	mlx_t	*mlx 
+
+	mlx = param;
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
 }
+
+//-------------------------------------------------------------------------
 
 int32_t	main(void)
 {
-	mlx_set_setting(MLX_MAXIMIZED, false);
-	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "42Balls", true);
-	if (!mlx)
-		ft_error();
+	mlx_t	*mlx;
 
-	/* Do stuff */
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "TEST", true)))
+	{
+		puts(mlx_strerror(mlx_errno));
+		return (EXIT_FAILURE);
+	}
 
-	// Create and display the image.
-	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
-	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
-		ft_error();
-
-	// Even after the image is being displayed, we can still modify the buffer.
-	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
-
-	// Register a hook and pass mlx as an optional param.
-	// NOTE: Do this before calling mlx_loop!
-	mlx_loop_hook(mlx, ft_hook, mlx);
+	mlx_loop_hook(mlx, global_hook, mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
